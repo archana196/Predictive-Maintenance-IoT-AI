00c2b90c50cf0f2a82ee9acf934a5177d6fec3e7
archana196 <archanaveera1967@gmail.com>
2026-07-05 19:00:43 +0530

    Retrain LightGBM model with 11 essential features and integrate Flask prediction pipeline

--- utils/predictor.py
+++ utils/predictor.py
@@ -1,15 +1,31 @@
 import joblib
 import pandas as pd
 
-model = joblib.load("../model/model.pkl")
+# Load trained model
+model = joblib.load("models/model.pkl")
+
+# Load encoders
+encoders = joblib.load("models/encoders.pkl")
+
 
 def predict_machine(data):
-    """
-    Predict machine failure.
-    Input: pandas DataFrame
-    Output: prediction and probability
-    """
-    prediction = model.predict(data)
-    probability = model.predict_proba(data)
+
+    # Convert dictionary to DataFrame
+    df = pd.DataFrame([data])
+
+    # Encode categorical features
+    categorical_columns = [
+        "Type",
+        "Shift",
+        "Day_Type"
+    ]
+
+    for col in categorical_columns:
+        df[col] = encoders[col].transform(df[col])
+
+    # Predict
+    prediction = model.predict(df)[0]
+
+    probability = model.predict_proba(df)[0][1]
 
     return prediction, probability
